627c167cf5dc0778947144652772dccb5d1dd2d5
Chris Craft <ChrisJCraft@gmail.com>
2012-01-18 21:20:12 -0500

    first try at USB

---
+++ src/TestHarness/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            USBPower.USBPower.Find();
+            var isPowerOn = USBPower.USBPower.IsPowerOn();
+            USBPower.USBPower.SetPower(true);
+        }
+    }
+}
---
+++ src/USBPower/USBPower.cs
@@ -0,0 +1,26 @@
+﻿using USBHIDDRIVER;
+
+namespace USBPower
+{
+    public static class USBPower
+    {
+        public static USBInterface USBInterface { get; set; }
+
+        public static void Find()
+        {
+            USBInterface = new USBInterface("vid_067b", "pid_2303");
+            USBInterface.Connect();
+        }
+
+        public static bool IsPowerOn()
+        {
+            USBInterface.write(new byte[] {0xc0, 0x01, 0x0081, 0x0000, 0x0001});
+            return false;
+        }
+
+        public static void SetPower(bool turnOn)
+        {
+            USBInterface.write(new byte[] {0x40, 0x01, 0x0001, 0xa0, 0x0000});
+        }
+    }
+}
